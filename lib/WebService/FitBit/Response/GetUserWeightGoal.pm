12cd4d57dc323861e75611204d034d9ed20913d9
Kaitlyn Parkhurst <symkat@symkat.com>
2013-01-26 10:12:22 -0800

    GetWeightGoal now includes goal_weight, goal_start_date and goal_start_date

--- lib/WebService/FitBit/Response/GetUserWeightGoal.pm
+++ lib/WebService/FitBit/Response/GetUserWeightGoal.pm
@@ -4,12 +4,22 @@ with 'WebService::FitBit::Response';
 use WebService::FitBit::Response::GetUserWeight::Weight;
 use Data::Dumper;
 
-has goal => (
-    is => 'lazy',
-);
-
-sub _build_goal {
-    return shift->json->{goal};
+for ( qw( goal_weight goal_start_weight goal_start_date ) ) {
+    has $_ => ( is => "lazy" );
 }
+
+sub _build_goal_weight {
+    return shift->json->{goal}->{weight};
+}
+
+sub _build_goal_start_weight {
+    return shift->json->{goal}->{startWeight};
+}
+
+sub _build_goal_start_date {
+    return shift->json->{goal}->{startDate};
+}
+
+
 
 1;
