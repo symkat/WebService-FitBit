2551dbfa91b87a128ca79ff359d083325f74eaa4
Kaitlyn Parkhurst <symkat@symkat.com>
2013-02-23 16:09:04 -0500

    Updated CreateSubscription to not be COMPLETELY WRONG.

---
+++ lib/WebService/FitBit/Request/GetSubscriptions.pm
@@ -0,0 +1,69 @@
+package WebService::FitBit::Request::GetSubscriptions;
+use Moo;
+use Scalar::Util qw( looks_like_number );
+with 'WebService::FitBit::Request';
+
+has type => ( 
+    is => 'ro', 
+    default => sub { "GET" } 
+);
+
+has endpoint => ( 
+    is => 'lazy', 
+);
+
+has oauth_consumer_key => (
+    is => 'ro',
+);
+
+has oauth_consumer_secret => (
+    is => 'ro',
+);
+
+has format => ( 
+    is => 'ro', 
+    isa => sub { $_[0] =~ /^(json|xml)$/ }, 
+    default => sub { 'json' },
+);
+
+has collection => (
+    is => 'ro',
+    isa => sub { ! $_[0] or $_[0] =~ /^(activities|foods|sleep|body)$/ },
+    default => sub { "body" },
+);
+
+sub _build_endpoint {
+    my ( $self ) = @_;
+
+    if ( $self->collection )  {
+        return sprintf( "/1/user/-/%s/apiSubscriptions.%s", 
+            $self->collection, $self->format
+        );
+    }
+    return sprintf( "/1/user/-/apiSubscriptions/.%s", $self->format );
+}
+
+sub oauth_params {
+    return {};
+}
+
+sub post_arguments {
+    my ( $self ) = @_;
+
+    return [  ];
+}
+
+sub query_params {  };
+sub as_exception {
+    my ( $self ) = @_;
+
+    return {
+        type => $self->type,
+        endpoint => $self->endpoint,
+        oauth_consumer_key => $self->oauth_consumer_key,
+        format => $self->format,
+        collection => $self->collection,
+    };
+}
+
+1;
--- lib/WebService/FitBit/Response/CreateSubscription.pm
+++ lib/WebService/FitBit/Response/CreateSubscription.pm
@@ -7,18 +7,10 @@ for (qw(owner_id subscriber_id owner_type collection_type subscription_id)) {
     has $_ => ( is => "lazy" );
 }
 
-sub _build_owner_id        { shift->_get_token( "collectionType" ); }
-sub _build_subscriber_id   { shift->_get_token( "ownerId" ); }
-sub _build_owner_type      { shift->_get_token( "ownerType" ); }
-sub _build_collection_type { shift->_get_token( "collectionType" ); }
-sub _build_subscription_id { shift->_get_token( "subscriptionId" ); }
-
-sub _get_token {
-    my ( $self, $token ) = @_;
-
-    my ( $content ) = $self->get_tokens( $self->response->content, $token );
-    
-    return $content;
-}
+sub _build_owner_id        { shift->json->{"ownerId"}; }
+sub _build_subscriber_id   { shift->json->{"subscriberId"}; }
+sub _build_owner_type      { shift->json->{"ownerType"}; }
+sub _build_collection_type { shift->json->{"collectionType"}; }
+sub _build_subscription_id { shift->json->{"subscriptionId"}; }
 
 1;
